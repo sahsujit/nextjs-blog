1fff35557a9e7c20ad1ec3b21cc58bb6f849a5b9
sujit sah <sujit8877123sah@gmail.com>
2024-09-16 22:05:14 +0545

    updated

--- app/about/page.js
+++ app/about/page.js
@@ -1,4 +1,84 @@
 
+// import Image from 'next/image';
+
+// export default function About() {
+//   return (
+//     <div>
+//       <div className="flex flex-col items-center justify-center py-32 bg-gray-100 dark:bg-gray-700 ">
+//         <div className="w-full max-w-4xl p-8 bg-white dark:bg-gray-800 shadow-lg rounded-lg">
+//           <div className="flex flex-col md:flex-row">
+//             <div className="w-full md:w-1/3 flex justify-center items-center mb-8 md:mb-0 ">
+//               <div className="relative w-48 h-48 rounded-full overflow-hidden">
+//                 <Image
+//                   src="/logo.jpeg"
+//                   alt="Profile"
+//                   layout="fill"
+//                   objectFit="cover"
+//                   className="rounded-full"
+//                 />
+//               </div>
+//             </div>
+//             <div className="w-full md:w-2/3 flex flex-col justify-center ">
+//               <h1 className="text-4xl font-bold text-gray-800 dark:text-white mb-4">About Me</h1>
+//               <p className="text-gray-600 dark:text-gray-50 text-lg mb-4">
+//                 Hello! I&apos;m Sujit, a passionate web developer . I created this blog to enjoy coding journey.
+//               </p>
+//               <p className="text-gray-600 dark:text-gray-50 text-lg">
+//                 Whether you&apos;re just starting out or looking to sharpen your skills, you&apos;ll find a variety of resources and insights here. Let&apos;s explore the world of programming together!
+//               </p>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+
+//       <section className="py-16 bg-gray-50 dark:bg-gray-800 dark:text-white">
+//         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+//           <div className="text-center mb-12">
+//             <h2 className="text-3xl font-extrabold text-gray-900 dark:text-white">Sujit's Journey as a Coder</h2>
+//             <p className="mt-4 text-lg text-gray-600 dark:text-gray-50">
+//               From curious beginner to seasoned developer, here&apos;s how Sujit navigated the world of programming.
+//             </p>
+//           </div>
+//           <div className="space-y-12">
+//             <div className="flex flex-col md:flex-row items-center">
+//               <div className="md:w-1/3">
+//                 <img src="/3.jpeg" alt="Sujit as a beginner" className="w-full rounded-lg shadow-lg" />
+//               </div>
+//               <div className="md:w-2/3 md:pl-8 mt-8 md:mt-0">
+//                 <h3 className="text-2xl font-bold text-gray-800  dark:text-white">The Spark of Curiosity</h3>
+//                 <p className="mt-4 text-gray-600 dark:text-gray-50">
+//                   Sujit&apos;s coding journey began in  school when he stumbled upon his first programming language—Python. What started as a simple curiosity quickly turned into a passion, as Sujit spent countless hours experimenting with code, building small projects, and learning the fundamentals of web development.
+//                 </p>
+//               </div>
+//             </div>
+
+//             <div className="flex flex-col md:flex-row-reverse items-center">
+//               <div className="md:w-1/3">
+//                 <img src="/profile.jpg" alt="Sujit learning new skills" className="w-full rounded-lg shadow-lg" />
+//               </div>
+//               <div className="md:w-2/3 md:pr-8 mt-8 md:mt-0">
+//                 <h3 className="text-2xl font-bold text-gray-800 dark:text-white">Diving Deeper</h3>
+//                 <p className="mt-4 text-gray-600 dark:text-gray-50">
+//                   After mastering the basics, Sujit&apos;s thirst for knowledge grew. He began exploring more complex topics such as web development. Enrolling in online courses , Sujit quickly expanded his skill set, taking on freelance projects to apply his knowledge in real-world scenarios.
+//                 </p>
+//               </div>
+//             </div>
+
+          
+//           </div>
+//         </div>
+//       </section>
+
+//     </div>
+//   );
+// }
+
+
+
+
+
+
+
 import Image from 'next/image';
 
 export default function About() {
@@ -21,7 +101,7 @@ export default function About() {
             <div className="w-full md:w-2/3 flex flex-col justify-center ">
               <h1 className="text-4xl font-bold text-gray-800 dark:text-white mb-4">About Me</h1>
               <p className="text-gray-600 dark:text-gray-50 text-lg mb-4">
-                Hello! I&apos;m Sujit, a passionate web developer . I created this blog to enjoy coding journey.
+                Hello! I&apos;m Sujit, a passionate web developer. I created this blog to enjoy my coding journey.
               </p>
               <p className="text-gray-600 dark:text-gray-50 text-lg">
                 Whether you&apos;re just starting out or looking to sharpen your skills, you&apos;ll find a variety of resources and insights here. Let&apos;s explore the world of programming together!
@@ -34,7 +114,7 @@ export default function About() {
       <section className="py-16 bg-gray-50 dark:bg-gray-800 dark:text-white">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="text-center mb-12">
-            <h2 className="text-3xl font-extrabold text-gray-900 dark:text-white">Sujit's Journey as a Coder</h2>
+            <h2 className="text-3xl font-extrabold text-gray-900 dark:text-white">Sujit&apos;s Journey as a Coder</h2>
             <p className="mt-4 text-lg text-gray-600 dark:text-gray-50">
               From curious beginner to seasoned developer, here&apos;s how Sujit navigated the world of programming.
             </p>
@@ -42,33 +122,42 @@ export default function About() {
           <div className="space-y-12">
             <div className="flex flex-col md:flex-row items-center">
               <div className="md:w-1/3">
-                <img src="/3.jpeg" alt="Sujit as a beginner" className="w-full rounded-lg shadow-lg" />
+                <Image 
+                  src="/3.jpeg" 
+                  alt="Sujit as a beginner" 
+                  width={500} 
+                  height={500} 
+                  className="w-full rounded-lg shadow-lg"
+                />
               </div>
               <div className="md:w-2/3 md:pl-8 mt-8 md:mt-0">
                 <h3 className="text-2xl font-bold text-gray-800  dark:text-white">The Spark of Curiosity</h3>
                 <p className="mt-4 text-gray-600 dark:text-gray-50">
-                  Sujit&apos;s coding journey began in  school when he stumbled upon his first programming language—Python. What started as a simple curiosity quickly turned into a passion, as Sujit spent countless hours experimenting with code, building small projects, and learning the fundamentals of web development.
+                  Sujit&apos;s coding journey began in school when he stumbled upon his first programming language—Python. What started as a simple curiosity quickly turned into a passion, as Sujit spent countless hours experimenting with code, building small projects, and learning the fundamentals of web development.
                 </p>
               </div>
             </div>
 
             <div className="flex flex-col md:flex-row-reverse items-center">
               <div className="md:w-1/3">
-                <img src="/profile.jpg" alt="Sujit learning new skills" className="w-full rounded-lg shadow-lg" />
+                <Image 
+                  src="/profile.jpg" 
+                  alt="Sujit learning new skills" 
+                  width={500} 
+                  height={500} 
+                  className="w-full rounded-lg shadow-lg"
+                />
               </div>
               <div className="md:w-2/3 md:pr-8 mt-8 md:mt-0">
                 <h3 className="text-2xl font-bold text-gray-800 dark:text-white">Diving Deeper</h3>
                 <p className="mt-4 text-gray-600 dark:text-gray-50">
-                  After mastering the basics, Sujit&apos;s thirst for knowledge grew. He began exploring more complex topics such as web development. Enrolling in online courses , Sujit quickly expanded his skill set, taking on freelance projects to apply his knowledge in real-world scenarios.
+                  After mastering the basics, Sujit&apos;s thirst for knowledge grew. He began exploring more complex topics such as web development. Enrolling in online courses, Sujit quickly expanded his skill set, taking on freelance projects to apply his knowledge in real-world scenarios.
                 </p>
               </div>
             </div>
-
-          
           </div>
         </div>
       </section>
-
     </div>
   );
 }
